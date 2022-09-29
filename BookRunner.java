428e2632d605bb4f7ee72097974253211052ad44
KevinJ6219 <114585625+KevinJ6219@users.noreply.github.com>
2022-09-29 19:28:46 -0400

    Delete BookRunner.java

--- BookRunner.java
+++
@@ -1,9 +0,0 @@
-
-public class BookRunner {
-    public static void main(String[] args){
-
-        Book book1 = new Book("Curious George", 50,4.5);
-        book1.Info();
-        book1.Rating();
-    }
-}
